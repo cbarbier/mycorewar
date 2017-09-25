3b5c316f5034dc98fea1cdb88b62f9c77ce4cd3d
Arthur GIULIANO <agiulian@e1r4p9.42.fr>
2017-09-25 18:06:55 +0200

    some diff fixed

--- core_srcs/ops/f_and.c
+++ core_srcs/ops/f_and.c
@@ -6,27 +6,11 @@
 /*   By: cbarbier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 18:17:05 by cbarbier          #+#    #+#             */
-/*   Updated: 2017/09/21 16:37:00 by agiulian         ###   ########.fr       */
+/*   Updated: 2017/09/25 18:04:36 by agiulian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
-
-static void	put_type(t_proc *proc, int val, int i)
-{
-	if (proc->ptype[i] == T_REG)
-		ft_printf("%d ", proc->reg[val - 1]);
-	else
-		ft_printf("%d ", val);
-}
-
-static void	print(t_proc *proc, int i_reg, int p0, int p1)
-{
-	ft_printf("P %4d | and ", proc->id);
-	put_type(proc, p0, 0);
-	put_type(proc, p1, 1);
-	ft_printf("r%d\n", i_reg + 1);
-}
 
 int		f_and(t_vm *vm, t_proc *proc)
 {
@@ -40,12 +24,8 @@ int		f_and(t_vm *vm, t_proc *proc)
 		return (0);
 	i_reg = proc->param[2] - 1;
 	if (vm->verbose & 4)
-		print(proc, i_reg, proc->param[0], p1);
+		ft_printf("P %4d | and %d %d r%d\n", proc->id, p0, p1, i_reg + 1);
 	proc->reg[i_reg] = p0 & p1;
 	proc->carry = !(proc->reg[i_reg]);
 	return (1);
 }
-/*
-		ft_printf("P %4d | and %d %d r%d\n", proc->id,
-				p0, p1, i_reg + 1);
-				*/
